aafb498b686cd8e0f17089fe74282b7a679b5280
mikegough <mike.gough@consbio.org>
2017-04-28 12:02:47 -0700

    Non JQuery Version

--- L.Control.Shapefile.js
+++ L.Control.Shapefile.js
@@ -9,7 +9,7 @@ L.Control.Shapefile = L.Control.extend({
         var form = L.DomUtil.create('div', 'leaflet-control-command-form', controlUI);
 
         // Insert the form HTML into the form.
-        form.innerHTML = "<form id='uploadForm' action='' method='post' enctype='multipart/form-data'><input id='file' type='file' name='uploadFile' style='display:none'></form>";
+        form.innerHTML = "<form id='uploadForm' action='' method='post' enctype='multipart/form-data'><input id='file' type='file' onchange='fileToArrayBuffer(event)' name='uploadFile' style='display:none'></form>";
 
         L.DomEvent
             .addListener(form, 'click', function () {
@@ -29,36 +29,37 @@ L.control.shapefile = function(opts) {
 // Add to map here or in a separate leaflet js file. Will need to change the order of the map.js and L.Control.Shapefile.js
 // L.control.shapefile({ position: 'topleft' }).addTo(map);
 
-window.onload = function() {
-// When the user uploads a file, convert the file to an Array Buffer.
-    $("input[name='uploadFile']").change(function () {
+// When the user uploads a file, convert the file to an array buffer.
+function fileToArrayBuffer(event) {
 
-        var file = $('#file').prop('files')[0];
-        var reader = new FileReader();
+    var file = event.target.files[0];
+    var reader = new FileReader();
 
-        reader.onloadend = function (e) {
-            console.log(e.target.result);
-            console.log(e.target.result.byteLength);
+    reader.onloadend = function (e) {
+        console.log(e.target.result);
+        console.log(e.target.result.byteLength);
 
-            // Pass the Array buffer to the shapfile-js function.
-            loadArrayBuffer(e.target.result);
-        };
+        // Pass the Array buffer to the shapfile-js funct
+        loadArrayBuffer(e.target.result);
+    };
 
-        reader.readAsArrayBuffer(file);
+    reader.readAsArrayBuffer(file);
 
-    });
 
-// Convert the array buffer to geojson and add it to the map
-    function loadArrayBuffer(buffer) {
+}
 
-        shp(buffer).then(function (geojson) {
-            var layer = L.geoJSON(geojson);
-            var layers = layer._layers;
-            $.each(layers, function () {
-                layer.addTo(map);
-                //initializePolygon(this);
-            });
+// Convert the array buffer to geojson and add it to the map as a layer
+function loadArrayBuffer(buffer) {
+
+    shp(buffer).then(function (geojson) {
+        var layer = L.geoJSON(geojson);
+        var layers = layer._layers;
+        console.log(layers)
+        Object.keys(layers).forEach(function(key) {
+            var layer = (layers[key]);
+            layer.addTo(map);
+
         });
-    }
+    });
+}
 
-};
